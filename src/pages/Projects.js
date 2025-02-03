94922054b6af07969162b8841f1389849dec26dc
Siphe23 <siphesihlemzamane@gmail.com>
2025-02-03 13:47:48 +0200

    Added files

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -23,7 +23,7 @@ const Projects = () => {
       keyFeatures: ['Real-time weather data', 'Search by city or location', 'Responsive design'],
       challenges: ['Handling API rate limits', 'Ensuring mobile responsiveness'],
       repoLink: 'https://github.com/Siphe23/weather-app.git',
-      deployedLink: 'https://weather-jn5n9alqa-nasiphis-projects-f287cdf3.vercel.app/login',
+      deployedLink: 'weather-app-git-main-nasiphis-projects-f287cdf3.vercel.app',
     },
     {
       id: 2,
